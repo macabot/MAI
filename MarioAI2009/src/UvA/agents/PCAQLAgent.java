31253afdd79c230e97e039e24660a465c49e442a
Michael Cabot <michael.a.cabot@gmail.com>
2013-01-17 11:59:11 +0100

    Remove SaveStatesAgent. Add PCAQLAgent: q-learn agent that reduces the state space with PCA

---
+++ MarioAI2009/src/UvA/agents/PCAQLAgent.java
@@ -0,0 +1,12 @@
+package UvA.agents;
+
+import java.util.List;
+
+import UvA.stateSpaceReduction.PCAMeans;
+
+public class PCAQLAgent extends QLearnAgent 
+{
+	List<State> visitedStates;
+	PCAMeans pcam;
+
+}
