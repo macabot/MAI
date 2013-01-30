12436365bc27893fd00dd980687c0c4b6c84da75
samkatt <sammie.katt@gmail.com>
2013-01-30 13:36:17 +0100

    EvaluatePCA without loading ser files

--- MarioAI2009/src/ch/idsia/scenarios/EvaluatePCA.java
+++ MarioAI2009/src/ch/idsia/scenarios/EvaluatePCA.java
@@ -1,15 +1,12 @@
 package ch.idsia.scenarios;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import UvA.agents.Calculate;
 import UvA.agents.PCAQLAgent;
-import UvA.agents.SLAPI;
+import UvA.agents.QLearnAgent;
 import UvA.stateSpaceReduction.PCAMeans;
 import UvA.states.PCAState;
-import UvA.states.StateActionPair;
 import ch.idsia.ai.tasks.ProgressTask;
 import ch.idsia.ai.tasks.Task;
 import ch.idsia.tools.CmdLineOptions;
@@ -46,10 +43,7 @@ public class EvaluatePCA {
 	 * @since   iMario1.0
 	 */
 
-	private static String loadPath = null;
-	private static String savePath = null;
-	private static boolean save = false;
-
+	// significant variables/settings for testing
 	private static int amountTrain = 1000;
 
 	// Set these for plotting
@@ -65,29 +59,45 @@ public class EvaluatePCA {
 	public Properties properties = new Properties();
 
 
-	@SuppressWarnings("unchecked") // hack to remove annoying warning of casting
 	public static void main(String[] args) {
 
-		//read properties
+		//read/set properties
 		String configFilePath = System.getProperty("user.dir") + "/config.properties";
 		Properties properties = PCAState.readPropertiesFile(configFilePath);
+		
+		//////////////// get states by playing with regular qlearnagent
 
-		//set all properties
-		//QLearnAgent.setAllProperties(properties);
-		//PCAQLAgent.setAllProperties(properties);
-		setAllProperties(properties); // for Evaluation
+		// set options
+		EvaluationOptions learnOptions = new CmdLineOptions(args);
+		Task learnTask = new ProgressTask(learnOptions);
+		QLearnAgent learnAgent = (QLearnAgent) learnOptions.getAgent();
 
-		// initialization
+		learnOptions.setLevelDifficulty(1);
+		learnOptions.setLevelRandSeed(15);      
+
+		learnOptions.setVisualization(false);
+		learnOptions.setMaxFPS(true);
+		learnTask.setOptions(learnOptions);
+
+		learnAgent.setAllProperties(properties);
+
+		// learn and extract Qvalues (indirectly the states)
+		for(int i = 0; i < amountTrain; i++) {
+			learnTask.evaluate(learnAgent);
+		} // end training
+		
+		double[][] states = PCAQLAgent.extractRepresentations(learnAgent.getQValues());
+		///////////////// end getting states
+
+
+		// set options for testing
 		EvaluationOptions options = new CmdLineOptions(args);
 		Task task = new ProgressTask(options);
 		PCAQLAgent agent = (PCAQLAgent) options.getAgent();
 
-		// regular options
 		options.setLevelDifficulty(1);
 		options.setLevelRandSeed(15);      
-		//options.setLevelLength(2560);
 
-		// set options specific for learning
 		options.setVisualization(false);
 		options.setMaxFPS(true);
 		task.setOptions(options);
@@ -98,55 +108,36 @@ public class EvaluatePCA {
 		double[][] tmpDistance = new double[episodes][amountEval];
 		double[][] tmpReward = new double[episodes][amountEval];
 
-		// TODO: create states
-		// TODO: initiate alpha, initial value and viewdim in config file 
-		// TODO: SET NRCOMPONENTS IN CONFIG FILE
-		// TODO: set correct loadPath in config.properties
-
-		Map<StateActionPair, Double> qValuesTemp = null;
-		try {
-			qValuesTemp = (HashMap<StateActionPair, Double>) SLAPI.load(loadPath);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);		// stop program
-		}
-
-		double[][] states = PCAQLAgent.extractRepresentations(qValuesTemp);
-
 		int[] nrClusters = {-1, states.length/100, states.length/10}; 
-		// nrCompononents:
-		// ? Sammie
-		// ? Anna
-		// ? Richard
-		// ? SP
+		// other tested variable is number of components, individually set on every machine
+		
+		// testing: for each cluster
 		for (int nrCluster = 0; nrCluster< nrClusters.length; nrCluster++) {
 
 			agent.setClusterAmount(nrClusters[nrCluster]);
-			
-			// recalculate clusters
+
+			// set clusters
 			System.out.print("Clustering....");
 			PCAMeans pcam = new PCAMeans(states, agent.getNumComponents(), agent.getClusterAmount(), agent.getIterations());
 			System.out.println("Done!");
-			
+
 			agent.setPCAM(pcam);
 
+			// test 
 			for (int ep=0; ep < episodes; ep++){
 				long start = System.currentTimeMillis();
-				
+
 				System.out.print("Episode: " + ep + 
 						" amount of clusters " + nrClusters[nrCluster] + 
 						"... ");
-				
-				
+
 				// reset qValues
 				agent.resetQValues();
 
-				
 				try{ 
 					for (int i = 0; i <= amountTrain; i++) { 
 						// set values
 
-						
 						task.evaluate(agent);
 
 						int modI = i%steps;
@@ -180,11 +171,11 @@ public class EvaluatePCA {
 				{
 					e.printStackTrace();
 				}// end catch
-				
+
 				System.out.printf("This episode took %d miliseconds \n", System.currentTimeMillis() - start);
-				
+
 			}// end for episodes
-			
+
 			// add tmpDistance[ep]
 			double[] averageDistance = Calculate.mean(tmpDistance);
 			double[] stdDistance = Calculate.standardDeviation(tmpDistance, averageDistance);
@@ -195,10 +186,6 @@ public class EvaluatePCA {
 			printStdDistance = stdDistance;
 			printAverageReward = averageReward;
 			printStdReward = stdReward;
-
-			// saving values
-			if(save)
-				agent.writeQValues(savePath);
 
 			// printing/saving stuff
 			double[][] toPrint = new double[4][printAverageDistance.length];
@@ -214,13 +201,5 @@ public class EvaluatePCA {
 		} // end loop nrClusters
 	}// end main
 
-	/**
-	 * Set all the properties via a config.properties file
-	 * @param properties
-	 */
-	public static void setAllProperties(Properties properties){
-		loadPath = System.getProperty("user.dir") + "/" + properties.getProperty("loadPath");
-		savePath = System.getProperty("user.dir") + "/" + properties.getProperty("savePath");
-	}//end function setAllProperties
 
 }// end class Play
